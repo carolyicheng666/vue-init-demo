d9e45253ab5f37905c4ec787df5a9569c5337832
Yicheng <790496165@qq.com>
2018-07-06 23:09:51 +0800

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# vue-init-demo
+
+Vue2 + Vue Router + webpack3 
+
+## Install
+```bash
+// install dependencies
+npm install
+```
+## Run
+### Development
+```bash
+// For the first time, run init to create index.html
+npm run init
+npm run dev
+```
+### Production(Build)
+```bash
+npm run build
+```
+
---
+++ src/views/index.vue
@@ -0,0 +1,23 @@
+<style scoped lang="less">
+.index {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 200px;
+  border: 1px solid #ccc;
+  & span {
+    font-size: 32px;
+    font-weight: bold;
+    color: #f00;
+  }
+}
+</style>
+<template>
+  <div class="index">
+    <span>index</span>
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
